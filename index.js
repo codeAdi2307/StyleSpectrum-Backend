7a36496c6a0a4b28bc07e4b6a19ff2924537cc49
codeAdi2307 <aditya07kl@gmail.com>
2024-06-10 06:35:02 +0530

    changes

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use("/api/v1/brand", brandRouter);
 app.use("/api/v1/cpn", couponRouter);
 
 // For Web API
-app.use("/api/v1", apiRouter);
+app.use(apiRouter);
 
 // For payment gateways
 app.use(cashfreeRouter);
